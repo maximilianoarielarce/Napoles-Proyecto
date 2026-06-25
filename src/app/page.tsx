81301a18a20298572ddbe09a49cf2d6e34e103a2
maximilianoarielarce <maximilianoarielarce@hotmail.com>
2026-06-24 21:40:49 -0300

    Fix build: comentar imports de secciones deshabilitadas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import HomeHero from '@/components/home-page/Hero';
-import HomeMenu from '@/components/home-page/Menu';
+// import HomeMenu from '@/components/home-page/Menu';
 import HomeSeasons from '@/components/home-page/Seasons';
 import HomeAmbience from '@/components/home-page/Ambience';
-import HomeSpecials from '@/components/home-page/Specials';
+// import HomeSpecials from '@/components/home-page/Specials';
 import HomeEvents from '@/components/home-page/Events';
 import HomeTestimonials from '@/components/home-page/Testimonials';
 import 'swiper/css';
 import { Metadata } from 'next';
-import { foodItemsData, homeData, siteName } from '@/data/content';
+import { homeData, siteName } from '@/data/content';
 
 export async function generateMetadata(): Promise<Metadata> {
     const pageTitle = `${homeData.seo.title} - ${siteName}`;
